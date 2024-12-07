6341a1c3ab64dd54615c039638ee43dbfbaa1c24
Alex N <alexaderdnguyen@gmail.com>
2024-12-07 17:48:21 -0500

    Update secrets_manager.py

--- secrets_manager.py
+++ secrets_manager.py
@@ -26,15 +26,3 @@ def get_service_secrets(service_name):
     return secrets.get(service_name, {})
     '''
 
-    # For local testing with AWS RDS
-    if service_name == 'gnosis-user-registration':
-        return {
-            "MYSQL_USER": "admin",
-            "MYSQL_PASSWORD_USERS": "IbU1Y5pFflqrrzduf7oC",
-            "MYSQL_HOST": "users-db.cffwkffbiore.us-east-1.rds.amazonaws.com",
-            "MYSQL_PORT": "3306",
-            "MYSQL_DATABASE": "user_db",
-            "PORT": "5000",
-            "JWT_SECRET_KEY": "BzR9J-Gn7-eiCq6PDP_yriiq_wRQ"
-        }
-    return {}
